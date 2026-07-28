842b5f8c1958fbb6883f3d3390ed14ecc7b1be4a
gleb <smirnovg2005@gmail.com>
2026-07-28 14:07:22 +0300

    fix: pm2 entrypoint path dist/src/main.js

--- glamping-backend/ecosystem.config.js
+++ glamping-backend/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'glamping-backend',
-    script: 'dist/main.js',
+    script: 'dist/src/main.js',
     instances: 1,
     exec_mode: 'fork',
     env_file: '.env.production',
